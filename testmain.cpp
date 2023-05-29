099a671dd1c43bf662a844266c0b09d79b511fe4
15011238T <1172962943@qq.com>
2023-05-30 00:26:00 +0800

    Correct test data

--- testmain.cpp
+++ testmain.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "data.cpp"
 #include "Eigen/Dense"
+#include <Eigen/Geometry>
 #include <opencv2/calib3d.hpp>
+#include <random>
 
 cv::Mat Matrix2Mat(Eigen::MatrixXd input ){
     int row = input.rows();
@@ -42,7 +44,7 @@ cv::Mat Eigen2Opencv44(const Eigen::Matrix4d & input){
     return ret;
 }
 
-void cvRttoH(cv::Mat H , const cv::Mat& R , const cv::Mat&t){
+void cvRttoH(cv::Mat &H , const cv::Mat& R , const cv::Mat&t){
     H = cv::Mat::eye(4,4,CV_64F);
     R.copyTo(H(cv::Rect(0,0,3,3)));
     t.copyTo(H(cv::Rect(3,0,1,3)));
@@ -66,21 +68,6 @@ void manyHtoRt(const std::vector<cv::Mat> & H ,
     }
 }
 
-
-void testHcij(std::vector<cv::Mat>& Hcij ){
-    Robot a1(10 , 15 , 20  , 20 , 30 ,30);
-    Robot a2(30 , 45 , 45  , 30 , 45 ,45);
-    Robot a3(60 , 60 , 60  , 45 , 60 ,60);
-    
-    cv::Mat cva1 = Eigen2Opencv44(a1.T);
-    cv::Mat cva2 = Eigen2Opencv44(a2.T);
-    cv::Mat cva3 = Eigen2Opencv44(a3.T);
-
-    Hcij.push_back(cva1);
-    Hcij.push_back(cva2);
-    Hcij.push_back(cva3);
-}
-
 cv::Mat invH(cv::Mat H){
     cv::Mat ans = cv::Mat::eye(4,4,CV_64F);
     cv::Mat R,t;
@@ -93,49 +80,69 @@ cv::Mat invH(cv::Mat H){
     return ans;
 }
 
-std::vector<cv::Mat> testHgij(const cv::Mat T ,std::vector<cv::Mat>& Hcij ){
-    int n = Hcij.size();
-    std::vector<cv::Mat> ret;
-    cv::Mat T_inv = invH(T);
-    for(int i = 0 ; i<n ; i++){
 
-        cv::Mat temp = (T * Hcij[i]) * T_inv;
-        ret.push_back(temp);
-    }
-    return ret;
+Eigen::Matrix3d generateRandomOrthogonalMatrix3D()
+{
+    Eigen::Quaterniond quaternion = Eigen::Quaterniond::UnitRandom();
+    Eigen::Matrix3d rotation_matrix = quaternion.toRotationMatrix();
+    return rotation_matrix;
+}
+
+Eigen::Vector3d generateRandomVector3D()
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dist(-100, 100);
+
+    Eigen::Matrix<double , 3,1> random_vector;
+    random_vector << dist(gen),
+                    dist(gen),
+                    std::abs(dist(gen));
+    
+    return random_vector;
 }
 
 
 int main(int argc , char ** argv){
 
     Robot Hcg_ans = Robot(50 , 100 , 150 ,30 , 0 , 0);
+    Robot Hbt_ans = Robot(500 , 300 , 2000 , 0 , 0 , 0);
     cv::Mat cvHcg_ans = Matrix2Mat(Hcg_ans.T);
+    cv::Mat cvHbt_ans = Matrix2Mat(Hbt_ans.T);
+    cv::Mat cvRcg_ans , cvtcg_ans , cvRbt_ans , cvtbt_ans;
 
-    std::vector<cv::Mat> Hcij ,Hgij;
-    testHcij(Hcij);
-    Hgij = testHgij(cvHcg_ans , Hcij);
+    cvHtoRt(cvHcg_ans , cvRcg_ans , cvtcg_ans);
+    cvHtoRt(cvHbt_ans , cvRbt_ans , cvtbt_ans);    
 
-    std::vector<cv::Mat> Rcij , Rgij , tcij , tgij;
-    for(int i = 0 ; i< Hcij.size() ; i++){
-        cv::Mat tempRc , temptc , tempRg , temptg;
-        cvHtoRt(Hcij[i] , tempRc , temptc);
-        Rcij.push_back(tempRc);
-        tcij.push_back(temptc);
-        cvHtoRt(Hgij[i] , tempRg , temptg);
-        Rgij.push_back(tempRg);
-        tgij.push_back(temptg);
+    std::vector<cv::Mat> Rcs , Rgs , tcs , tgs;
+    int data_number = 3;
+    for(int i = 0 ; i<data_number ; i++){
+        Eigen::Matrix3d tempR = generateRandomOrthogonalMatrix3D();
+        Eigen::Matrix<double ,3,1> tempt = generateRandomVector3D();
+        
+        cv::Mat Rc , Rg , tc , tg , Hg , Hc;
+        Rc = Matrix2Mat(tempR);
+        tc = Matrix2Mat(tempt);
+        Rcs.push_back(Rc);
+        tcs.push_back(tc);
+        cvRttoH(Hc , Rc , tc);
+        
+        cv::Mat tempH = cvHcg_ans * (Hc * cvHbt_ans);
+        Hg = invH(tempH);
+        // std::cout<<Hg<<std::endl;
+        cvHtoRt(Hg , Rg , tg);
+        Rgs.push_back(Rg);
+        tgs.push_back(tg);
+        // std::cout<<Rc <<tc<<std::endl;
+        // std::cout<<Rg <<tg<<std::endl;
     }
 
     cv::Mat cvRpark , cvtpark;
-    cv::calibrateHandEye(Rgij , tgij , Rcij , tcij , cvRpark , cvtpark , cv::CALIB_HAND_EYE_PARK);
-    
-    cv::Mat cvinvT_ans = invH(cvHcg_ans);
+    cv::calibrateHandEye(Rgs , tgs , Rcs , tcs , cvRpark , cvtpark , cv::CALIB_HAND_EYE_PARK);
 
-    // std::cout<<cvRpark<<std::endl;
-    // std::cout<<cvtpark<<std::endl;
-    // std::cout<<cvHcg_ans<<std::endl;
-    // std::cout<< Hgij[0] * cvHcg_ans<<std::endl;
-    // std::cout<< cvHcg_ans * Hcij[0]<<std::endl;
+    std::cout<<cvRpark<<std::endl;
+    std::cout<<cvtpark<<std::endl; 
+    std::cout<<cvHcg_ans<<std::endl;
 
     return 0;
 
